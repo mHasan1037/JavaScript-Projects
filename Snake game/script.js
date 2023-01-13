76755075c4602de9f8bf6f6626be503f4e7634a0
Mahmudul Hasan <112176017+mHasan1037@users.noreply.github.com>
2023-01-13 13:33:36 +0600

    Add files via upload

--- Snake game/script.js
+++ Snake game/script.js
@@ -12,72 +12,63 @@ let snakeArr = [
 
 food = {x: 6, y: 7}
 
-//Game Function
-function main(cTime){
+//Game Functions
+
+function main(ctime){
     window.requestAnimationFrame(main)
-    if((cTime - lastPaintTime) / 1000 < 1/speed){
-       return 
+    if((ctime - lastPaintTime) / 1000 < 1 / speed){
+        return;
     }
-    lastPaintTime = cTime
+    lastPaintTime = ctime
 
     gameEngine()
 }
 
 function isCollide(snake){
-    // If you bump into yourself
     for(let i = 1; i < snakeArr.length; i++){
         if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
             return true
-        }
-    }
-    // If you bump into the wall
+        }}
+    
     if(snake[0].x >= 18 || snake[0].x <= 0 || snake[0].y >= 18 || snake[0].y <= 0){
-        return true
+        return true;
     }
 }
 
 function gameEngine(){
-    // Updating the snake array & food
+
     if(isCollide(snakeArr)){
         gameOverSound.play()
         musicSound.pause()
-        inputDir = {x: 0, y: 0};
-        alert('Game Over. Press any key to play again!');
+        inputDir = {x: 0, y: 0}
+        window.alert('Game Over. Press any key to play again!')
         snakeArr = [{x: 13, y: 15}]
         musicSound.play()
         score = 0
     }
 
-    // If you have eaten the food, increment the score and regenerate the food
     if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
         foodSound.play()
-        score += 1;
-        if(score > hiscoreval){
-            hiscoreval = score
-            localStorage.setItem('hiscore', JSON.stringify(hiscoreval))
-            hiscoreBox.innerHTML = "High Score: " + hiscoreval
-        }
-        scoreBox.innerHTML = "Score: " + score;
+        score += 1
+        scoreBox.innerHTML = "Score: " + score
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y})
         let a = 2;
         let b = 16;
         food = {x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random())}
     }
 
-    //Moving the snake
     for(let i = snakeArr.length - 2; i >= 0; i--){
-        snakeArr[i + 1] = {...snakeArr[i]};
+        snakeArr[i + 1] = {...snakeArr[i]}
     }
 
-    snakeArr[0].x += inputDir.x
-    snakeArr[0].y += inputDir.y
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y;
 
-    // Display the snake
-    board.innerHTML = ""
-    snakeArr.forEach((e, index)=>{
+    board.innerHTML = '';
+    snakeArr.forEach((e, index) =>{
         snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = e.y
-        snakeElement.style.gridColumnStart = e.x
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
         if(index === 0){
             snakeElement.classList.add('head')
         }
@@ -85,29 +76,21 @@ function gameEngine(){
         board.appendChild(snakeElement)
     })
 
-    //Display the food
     foodElement = document.createElement('div')
-    foodElement.style.gridRowStart = food.y
-    foodElement.style.gridColumnStart = food.x
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
     foodElement.classList.add('food')
     board.appendChild(foodElement)
 }
 
 
 //Main logic starts here
-let hiscore = localStorage.getItem('hiscore')
-if(hiscore === null){
-    hiscoreval = 0
-    localStorage.setItem('hiscore', JSON.stringify(hiscoreval))
-}else{
-    hiscoreval = JSON.parse(hiscore)
-    hiscoreBox.innerHTML = "High Score:" + hiscore
-}
-
 window.requestAnimationFrame(main)
 window.addEventListener('keydown', e =>{
-    inputDir = {x: 0, y: 1} // Start the game
+    inputDir = {x: 0, y: 1}
+    musicSound.pause()
     moveSound.play()
+
     switch(e.key){
         case "ArrowUp":
             inputDir.x = 0;
@@ -129,8 +112,3 @@ window.addEventListener('keydown', e =>{
             break;
     }
 })
-
-
-
-
-
